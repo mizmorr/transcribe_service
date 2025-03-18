076d6269fb28295f9a17f6a66e11248759364c70
mizmorr <mizmorrr@gmail.com>
2025-03-19 01:55:49 +0300

    feat: add mp3 to wav converter

--- internal/transcriber/transcribe.go
+++ internal/transcriber/transcribe.go
@@ -1,14 +1,23 @@
 package transcriber
 
 import (
+	"errors"
 	"os/exec"
+	"strings"
+
+	"github.com/mizmorr/transcriber/pkg/utils"
 )
 
 type Transcriber struct{}
 
 func (t *Transcriber) Transcribe(filePath string, resultChan chan<- string, errChan chan<- string) {
 
-	cmd := exec.Command("python", "../internal/transcriber/transcribe.py", "../output.wav")
+	if err := t.preProcessing(filePath); err != nil {
+		errChan <- err.Error()
+		return
+	}
+
+	cmd := exec.Command("python", "../internal/transcriber/transcribe.py", filePath)
 
 	output, err := cmd.CombinedOutput()
 	if err != nil {
@@ -17,4 +26,33 @@ func (t *Transcriber) Transcribe(filePath string, resultChan chan<- string, errC
 	}
 
 	resultChan <- string(output)
+}
+
+func (t *Transcriber) preProcessing(filePath string) error {
+	isMp3, err := utils.IsMP3(filePath)
+
+	if err != nil {
+		return err
+	}
+
+	if isMp3 {
+		err = utils.ConvertMP3ToWAV(filePath, strings.Replace(filePath, ".mp3", ".wav", 1))
+
+		if err != nil {
+			return err
+		}
+	}
+
+	isWav, err := utils.IsWAV(filePath)
+
+	if err != nil {
+		return err
+
+	}
+
+	if !isWav {
+		return errors.New("unexpected audio format")
+	}
+
+	return nil
 }
